4ac08f1ad05f78f60c5fc0ca0629bc4f6978eb80
danicuba-stats <danielacuba7@gmail.com>
2021-11-11 13:34:08 +0000

    Initial commit :)

---
+++ Fit_NonSpatialExpCopulaModel.R
@@ -0,0 +1,23 @@
+### ======================================================================== ###
+### Fit Exponential Factor Copula Model to unreplicated dataset              ###
+### Daniela Cuba                                                             ###                                                      ###
+### ======================================================================== ###
+
+## Load libraries and functions ##
+library(parallel)
+source('./CensoredLikelihood_nonspatial.R')
+
+## Load data ##
+pair<-"PbCr"
+load("./pairs_uniform.Rdata")
+
+## Fit model to pair##
+ExpFac_models<- lapply(1:length(pair), function(x){
+  tmp<-tryCatch({fit.model.likelihood(pairs_uniform[[x]],thres=0.82,censorL=T,starting=T)},
+                error=function(cond){
+                  return(list(par=c(NA,NA),value=NA))
+                })
+  })
+
+## Save output ##
+# save(ExpFac_models,file="./ExpFac_models.RData")
